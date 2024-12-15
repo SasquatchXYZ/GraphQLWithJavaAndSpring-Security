4e6788ca009d7cdb4e85d99a1f6f747e6f805822
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-15 11:40:36 -0500

    Refactor controller

--- src/main/java/playground/security_demo/controllers/OrderController.java
+++ src/main/java/playground/security_demo/controllers/OrderController.java
@@ -3,32 +3,28 @@ package playground.security_demo.controllers;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import playground.security_demo.domain.DeleteOrderInput;
 import playground.security_demo.domain.DeleteOrderPayload;
 import playground.security_demo.domain.Order;
 import playground.security_demo.service.OrderService;
+import reactor.core.publisher.Mono;
 
-import java.nio.file.AccessDeniedException;
-import java.security.Principal;
 import java.util.List;
 
 @Controller
 public record OrderController(OrderService orderService) {
 
     @QueryMapping
-    List<Order> myOrders(Principal principal) {
-        return orderService.getOrdersByOwner(principal.getName());
+    Mono<List<Order>> myOrders() {
+        return orderService.getOrdersForCurrentUser();
     }
 
     @MutationMapping
-    DeleteOrderPayload deleteOrder(@Argument DeleteOrderInput input, Principal principal) throws AccessDeniedException {
-        UsernamePasswordAuthenticationToken user = (UsernamePasswordAuthenticationToken) principal;
-        if (!user.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
-            throw new AccessDeniedException("Only Admins can delete orders");
-        }
-        return new DeleteOrderPayload(orderService.deleteOrder(input.orderId()));
+    Mono<DeleteOrderPayload> deleteOrder(
+            @Argument DeleteOrderInput input
+    ) {
+        Mono<Boolean> success = orderService.deleteOrder(input.orderId());
+        return success.map(DeleteOrderPayload::new);
     }
 }
